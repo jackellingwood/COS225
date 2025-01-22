9eeaac0d247327e860f716f992e6315a2b1863f7
jackellingwood <ellingwoodjack@gmail.com>
2025-01-22 14:21:12 -0500

    homework0 submission

---
+++ Homework0/homework0.java
@@ -0,0 +1,8 @@
+package Homework0;
+
+public class homework0 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This code was written by Jack Ellingwood.");
+    }
+}
